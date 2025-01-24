d355ea90e44238c5dbf928740ec2b27be23b4b67
Kiernan Sharief <medaturd76@gmail.com>
2025-01-23 22:06:10 -0600

    BinaryAICompleted

--- AI_TOOLS/AI.py
+++ AI_TOOLS/AI.py
@@ -1,42 +1,12 @@
 import numpy as np
 
 class NeuronLayer():
-    def __init__(self, inputs, neurons):
-        self.neuronCount = neurons
-        self.weights = 0.01 * np.random.randn(inputs, neurons)
-        self.biases = np.zeros((1, neurons))
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
     def forward(self, inputs):
-        self.inputs = inputs
         self.output = np.dot(inputs, self.weights) + self.biases
 
-provided_inputs = [
-    [0, 0],
-    [1, 0],
-    [0, 1],
-    [1, 1]
-]
-
-expected_outputs = [
-    [0],
-    [1],
-    [1],
-    [0]
-]
-
-layer1 = NeuronLayer(2, 2)
-layer2 = NeuronLayer(2, 4)
-layer3 = NeuronLayer(4, 8)
-layer4 = NeuronLayer(8, 8)
-layer5 = NeuronLayer(8, 4)
-layer6 = NeuronLayer(4, 2)
-layer7 = NeuronLayer(2, 1)
-
-for i in range(4):
-    layer1.forward(provided_inputs[i])
-    layer2.forward(layer1.output)
-    layer3.forward(layer2.output)
-    layer4.forward(layer3.output)
-    layer5.forward(layer4.output)
-    layer6.forward(layer5.output)
-    layer7.forward(layer6.output)
-    print(layer7.output)
+class ActivationReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
---
+++ BinaryInterpreter.py
@@ -0,0 +1,113 @@
+import numpy as np
+import os
+from AI_TOOLS.AI import *
+
+def clear():
+    os.system( 'cls' )
+
+class ActivationSoftmax:
+    def forward(self, inputs):
+        self.output = inputs[0][0].__round__()
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        
+        return np.mean(sample_losses)
+    
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        losses = []
+        for o in range(samples):
+            losses.append(abs(y_true[o]-y_pred[o]))
+        return losses
+
+
+provided_inputs = [
+    [0, 0],
+    [1, 0],
+    [0, 1],
+    [1, 1]
+]
+
+expected_outputs = [
+    [0],
+    [1],
+    [1],
+    [0]
+]
+
+dense1 = NeuronLayer(2,4)
+activation1 = ActivationReLU()
+
+dense2 = NeuronLayer(4,4)
+activation2 = ActivationSoftmax()
+
+dense1.forward(provided_inputs)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+
+loss_function = Loss_CategoricalCrossentropy()
+
+lowest_loss = loss_function.calculate(dense2.output, expected_outputs)
+best_dense1_weights = dense1.weights.copy()
+best_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+best_dense2_biases = dense2.biases.copy()
+
+for iteration in range(100000):
+    if iteration%2000 == 0:
+        clear()
+        print("Training the AI! Progress:", iteration/1000)
+
+    dense1.weights += 0.05 * np.random.randn(2,4)
+    dense1.biases += 0.05 * np.random.randn(1,4)
+    dense2.weights += 0.05 * np.random.randn(4,1)
+    dense2.biases += 0.05 * np.random.randn(1,1)
+
+    dense1.forward(provided_inputs)
+    activation1.forward(dense1.output)
+    dense2.forward(activation1.output)
+    
+    loss = loss_function.calculate(dense2.output, expected_outputs)
+
+    if loss < lowest_loss:
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
+    
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense2.biases = best_dense2_biases.copy()
+
+clear()
+print("AI trained and ready!")
+
+running = True
+
+while running:
+    input1 = input("Please enter a single byte (1 or 0) or type exit:")
+    if input1 == "exit":
+        running = False
+        break
+    elif input1 == "1" or input1 == "0":
+        input2 = input("Please enter another single byte (1 or 0) or type exit:")
+        if input2 == "exit":
+            running = False
+            break
+        elif input2 == "1" or input2 == "0":
+            dense1.forward([int(input1), int(input2)])
+            activation1.forward(dense1.output)
+            dense2.forward(activation1.output)
+            activation2.forward(dense2.output)
+            print("Those two bites result in a ", activation2.output, "!")
+        else:
+            print("Your passed an invalid input. The input was ", input2, ". Restarting!")
+    else:
+        print("Your passed an invalid input. The input was ", input1, ". Restarting!")
